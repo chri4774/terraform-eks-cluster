8a8b09175bfec13d972324da41ff466c04898353
Christian Stoff <chri01@web.de>
2020-03-11 13:56:19 +0000

    Fixes

--- providers.tf
+++ providers.tf
@@ -2,12 +2,10 @@
 # Provider Configuration
 #
 
-#provider "aws" {
-#  region = "${var.region}"
-#  profile = "${var.aws_profile}"
-#}
-
-provider "aws" {}
+provider "aws" {
+  #region = "${var.region}"
+  profile = "${var.aws_profile}"
+}
 
 # Using these data sources allows the configuration to be
 # generic for any region.
