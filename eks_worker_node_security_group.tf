426a345f6ece2f2f68cf44d76832662b2c503d7a
Christian Stoff <chri01@web.de>
2020-03-11 14:54:40 +0000

    Changed Syntax to Terraform 0.12

--- eks_worker_node_security_group.tf
+++ eks_worker_node_security_group.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "demo-node" {
   name        = "terraform-eks-demo-node"
   description = "Security group for all nodes in the cluster"
-  vpc_id      = "${aws_vpc.demo.id}"
+  vpc_id      = aws_vpc.demo.id
 
   egress {
     from_port   = 0
@@ -10,20 +10,18 @@ resource "aws_security_group" "demo-node" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  tags = "${
-    map(
-     "Name", "terraform-eks-demo-node",
-     "kubernetes.io/cluster/${var.cluster-name}", "owned",
-    )
-  }"
+  tags = {
+    "Name" = var.cluster-name
+    "kubernetes.io/cluster/${var.cluster-name}" = "owned"
+  }
 }
 
 resource "aws_security_group_rule" "demo-node-ingress-self" {
   description              = "Allow node to communicate with each other"
   from_port                = 0
   protocol                 = "-1"
-  security_group_id        = "${aws_security_group.demo-node.id}"
-  source_security_group_id = "${aws_security_group.demo-node.id}"
+  security_group_id        = aws_security_group.demo-node.id
+  source_security_group_id = aws_security_group.demo-node.id
   to_port                  = 65535
   type                     = "ingress"
 }
@@ -32,18 +30,18 @@ resource "aws_security_group_rule" "demo-node-ingress-cluster" {
   description              = "Allow worker Kubelets and pods to receive communication from the cluster control plane"
   from_port                = 1025
   protocol                 = "tcp"
-  security_group_id        = "${aws_security_group.demo-node.id}"
-  source_security_group_id = "${aws_security_group.demo-cluster.id}"
+  security_group_id        = aws_security_group.demo-node.id
+  source_security_group_id = aws_security_group.demo-cluster.id
   to_port                  = 65535
   type                     = "ingress"
 }
 
 resource "aws_security_group_rule" "demo-node-ingress-ssh" {
-  description              = "Allow ssh-access to nodes"
-  from_port                = 22
-  protocol                 = "tcp"
-  security_group_id        = "${aws_security_group.demo-node.id}"
-  cidr_blocks              = ["89.1.172.182/32"]
-  to_port                  = 22
-  type                     = "ingress"
+  description       = "Allow ssh-access to nodes"
+  from_port         = 22
+  protocol          = "tcp"
+  security_group_id = aws_security_group.demo-node.id
+  cidr_blocks       = ["89.1.172.182/32"]
+  to_port           = 22
+  type              = "ingress"
 }
